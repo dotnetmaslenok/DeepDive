c6df4995dfc47839ff9a25c19c703ed4ca21ad0c
dotnetmaslenok <a.kudryavcev8080@gmail.com>
2022-12-23 21:22:51 +0300

    rabbitmq basics

---
+++ MicroserviceCommunications/CommunicationsThroughMessageQueue.ReceiverMicroservice/Program.cs
@@ -0,0 +1,239 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace CommunicationsThroughMessageQueue.ReceiverMicroservice;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		RabbitMQ_Topics_ReceiverConfiguration(args);
+	}
+
+	public static void RabbitMQ_HelloWorld_ReceiverConfiguration()
+	{
+		const string queue = "hello_world_queue";
+		var factory = new ConnectionFactory() { HostName = "localhost" };
+
+		using (var connection = factory.CreateConnection())
+		{
+			using (var channel = connection.CreateModel())
+			{
+				channel.QueueDeclare(
+					queue: queue,
+					durable: false,
+					exclusive: false,
+					autoDelete: false,
+					arguments: default);
+
+				var consumer = new EventingBasicConsumer(channel);
+				consumer.Received += (model, ea) =>
+				{
+					var body = ea.Body.ToArray();
+					var content = Encoding.UTF8.GetString(body);
+
+					Console.WriteLine($"Content from sender microservice: {content}");
+				};
+
+				channel.BasicConsume(
+					queue: queue,
+					autoAck: true,
+					consumer: consumer);
+
+				Console.WriteLine("Press [enter] to exit.");
+				Console.ReadLine();
+			}
+		}
+	}
+
+	public static void RabbitMQ_WorkQueues_ReceiverConfiguration()
+	{
+		const string queue = "work_queue";
+		var factory = new ConnectionFactory() { HostName = "localhost" };
+		using (var connection = factory.CreateConnection())
+		{
+			using (var channel = connection.CreateModel())
+			{
+				channel.QueueDeclare(
+					queue: queue,
+					durable: true,
+					exclusive: false,
+					autoDelete: false,
+					arguments: default);
+
+				channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+				Console.WriteLine("Waiting for messages");
+
+				var consumer = new EventingBasicConsumer(channel);
+
+				consumer.Received += (model, ea) =>
+				{
+					var body = ea.Body.Span;
+					var content = Encoding.UTF8.GetString(body);
+
+					Console.WriteLine($"Content from sender microservice: {content}");
+					var dots = content.Count(x => x == '.');
+					Thread.Sleep(1000 * dots);
+					Console.WriteLine($"Done after {dots} seconds");
+
+					channel.BasicAck(ea.DeliveryTag, false);
+				};
+
+				channel.BasicConsume(queue: queue, autoAck: false, consumer: consumer);
+
+				Console.WriteLine("Press [enter] to exit");
+				Console.ReadLine();
+			}
+		}
+	}
+
+	public static void RabbitMQ_SubscribePublish_ReceiverConfiguration()
+	{
+		const string exchangeName = "logs";
+		var factory = new ConnectionFactory() { HostName = "localhost" };
+
+		using (var connection = factory.CreateConnection())
+		{
+			using (var channel = connection.CreateModel())
+			{
+				channel.ExchangeDeclare(exchangeName, ExchangeType.Fanout);
+
+				var temporaryQueue = channel.QueueDeclare().QueueName;
+
+				channel.QueueBind(temporaryQueue, exchangeName, string.Empty);
+
+				Console.WriteLine("Waiting for logs");
+
+				var consumer = new EventingBasicConsumer(channel);
+				consumer.Received += (model, ea) =>
+				{
+					var body = ea.Body.Span;
+					var logs = Encoding.UTF8.GetString(body);
+					Console.WriteLine($"Logs from sender microservice: {logs}");
+				};
+
+				channel.BasicConsume(temporaryQueue, true, consumer);
+
+				Console.WriteLine("Press [enter] to exit");
+				Console.ReadLine();
+			}
+		}
+	}
+
+	public static void RabbitMQ_Routing_ReceiverConfiguration(string[] args)
+	{
+		const string exchangeName = "direct_logs";
+		var factory = new ConnectionFactory() { HostName = "localhost" };
+
+		using (var connection = factory.CreateConnection())
+		{
+			using (var channel = connection.CreateModel())
+			{
+				channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+
+				var temporaryQueue = channel.QueueDeclare().QueueName;
+
+				channel.QueueBind(temporaryQueue, exchangeName, string.Empty);
+
+				if (CheckInvalidArgsCount(args))
+				{
+
+				}
+
+				foreach (var logLevel in args)
+				{
+					channel.QueueBind(temporaryQueue, exchangeName, logLevel);
+				}
+
+				Console.WriteLine("Waiting for logs");
+
+				var consumer = new EventingBasicConsumer(channel);
+				consumer.Received += OnConsumerReceived;
+
+				channel.BasicConsume(temporaryQueue, true, consumer);
+
+				Console.WriteLine("Press [enter] to exit");
+				Console.ReadLine();
+
+				bool CheckInvalidArgsCount(string[] args)
+				{
+					if (args.Length >= 1)
+					{
+						return false;
+					}
+
+					Console.Error.WriteLine($"Usage {Environment.GetCommandLineArgs()[0]} [info] [warning] [error]");
+
+					Console.WriteLine("Press [enter] to exit");
+					Console.ReadLine();
+
+					Environment.ExitCode = 1;
+					return true;
+				}
+
+				void OnConsumerReceived(object? model, BasicDeliverEventArgs ea)
+				{
+					var body = ea.Body.Span;
+					var logs = Encoding.UTF8.GetString(body);
+					var routingKey = ea.RoutingKey;
+					Console.WriteLine($"Sent logs by routing key {routingKey}: {logs}");
+				}
+			}
+		}
+	}
+
+	public static void RabbitMQ_Topics_ReceiverConfiguration(string[] args)
+	{
+		const string exchangeName = "topic_logs";
+		var factory = new ConnectionFactory() { HostName = "localhost" };
+
+		using (var connection = factory.CreateConnection())
+		{
+			using (var channel = connection.CreateModel())
+			{
+				channel.ExchangeDeclare(exchangeName, ExchangeType.Topic);
+
+				var temporaryQueue = channel.QueueDeclare().QueueName;
+
+				if (args.Length < 1)
+				{
+					EmitErrorState();
+
+					return;
+				}
+
+				foreach (var bindingKey in args)
+				{
+					channel.QueueBind(temporaryQueue, exchangeName, bindingKey);
+				}
+
+				Console.WriteLine("Waiting for messages");
+
+				var consumer = new EventingBasicConsumer(channel);
+				consumer.Received += OnConsumerReceived;
+				channel.BasicConsume(temporaryQueue, true, consumer);
+
+				Console.WriteLine("Press [enter] to exit");
+				Console.ReadLine();
+
+				void EmitErrorState()
+				{
+					Console.Error.WriteLine($"Usage {Environment.GetCommandLineArgs()[0]} [binding key...]");
+					Console.WriteLine("Press [enter] to exit");
+					Console.ReadLine();
+					Environment.ExitCode = 1;
+				}
+
+				void OnConsumerReceived(object? model, BasicDeliverEventArgs ea)
+				{
+					var body = ea.Body.Span;
+					var content = Encoding.UTF8.GetString(body);
+					var routingKey = ea.RoutingKey;
+					Console.WriteLine($"Sent logs by routing key {routingKey}: {content}");
+				}
+			}
+		}
+	}
+}
---
+++ MicroserviceCommunications/CommunicationsThroughMessageQueue.SenderMicroservice/Program.cs
@@ -0,0 +1,164 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+namespace CommunicationsThroughMessageQueue.SenderMicroservice;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+		RabbitMQ_Topics_SenderConfiguration(args);
+	}
+
+	public static void RabbitMQ_HelloWorld_SenderConfiguration()
+	{
+		const string queue = "hello_world_queue";
+		var factory = new ConnectionFactory() { HostName = "localhost" };
+
+		using (var connection = factory.CreateConnection())
+		{
+			using (var channel = connection.CreateModel())
+			{
+				channel.QueueDeclare(
+					queue: queue,
+					durable: false,
+					exclusive: false,
+					autoDelete: false,
+					arguments: default);
+
+				var message = "hello world message";
+				var body = Encoding.UTF8.GetBytes(message);
+
+				channel.BasicPublish(
+					exchange: string.Empty,
+					routingKey: queue,
+					basicProperties: null,
+					body: body);
+
+				Console.WriteLine($"Sent message: {message}");
+			}
+
+			Console.WriteLine("Press [enter] to exit");
+			Console.ReadLine();
+		}
+	}
+
+	public static void RabbitMQ_WorkQueues_SenderConfiguration(string[] args)
+	{
+		const string queue = "work_queue";
+		var factory = new ConnectionFactory() { HostName = "localhost" };
+
+		using (var connection = factory.CreateConnection())
+		{
+			using (var channel = connection.CreateModel())
+			{
+				channel.QueueDeclare(
+					queue: queue,
+					durable: true,
+					exclusive: false,
+					autoDelete: false,
+					arguments: default);
+
+				var message = GetMessage(args);
+				var body = Encoding.UTF8.GetBytes(message);
+				var properties = channel.CreateBasicProperties();
+
+				properties.Persistent = true;
+
+				channel.BasicPublish(
+					exchange: string.Empty,
+					routingKey: queue,
+					properties,
+					body: body);
+
+				Console.WriteLine($"Sent message: {message}");
+
+				Console.WriteLine("Press [enter] to exit");
+				Console.ReadLine();
+			}
+		}
+	}
+
+	public static void RabbitMQ_SubscribePublish_SenderConfiguration(string[] args)
+	{
+		const string exchangeName = "logs";
+		var factory = new ConnectionFactory() { HostName = "localhost" };
+
+		using (var connection = factory.CreateConnection())
+		{
+			using (var channel = connection.CreateModel())
+			{
+				channel.ExchangeDeclare(exchangeName, ExchangeType.Fanout);
+
+				var message = GetMessage(args);
+				var body = Encoding.UTF8.GetBytes(message);
+
+				channel.BasicPublish(
+					exchange: exchangeName,
+					routingKey: string.Empty,
+					basicProperties: default,
+					body: body);
+
+				Console.WriteLine($"Sent message: {message}");
+
+				Console.WriteLine("Press [enter] to exit");
+				Console.ReadLine();
+			}
+		}
+	}
+
+	public static void RabbitMQ_Routing_SenderConfiguration(string[] args)
+	{
+		const string exchangeName = "direct_logs";
+		var factory = new ConnectionFactory() { HostName = "localhost" };
+
+		using (var connection = factory.CreateConnection())
+		{
+			using (var channel = connection.CreateModel())
+			{
+				channel.ExchangeDeclare(exchangeName, ExchangeType.Direct);
+
+				var logLevel = args.Length > 0 ? args[0] : "info";
+				var message = args.Length > 1 ? string.Join(" ", args.Skip(1).ToArray()) : "Hello World. (Default message)";
+
+				var body = Encoding.UTF8.GetBytes(message);
+				channel.BasicPublish(exchangeName, logLevel, default, body);
+
+				Console.WriteLine($"Sent message with log level {logLevel}: {message}");
+
+				Console.WriteLine("Press [enter] to exit");
+				Console.ReadLine();
+			}
+		}
+	}
+
+	public static void RabbitMQ_Topics_SenderConfiguration(string[] args)
+	{
+		const string exchangeName = "topic_logs";
+		var factory = new ConnectionFactory() { HostName = "localhost" };
+
+		using (var connection = factory.CreateConnection())
+		{
+			using (var channel = connection.CreateModel())
+			{
+				channel.ExchangeDeclare(exchangeName, ExchangeType.Topic);
+
+				var routingKey = args.Length > 0 ? args[0] : "anonymous.info";
+				var message = args.Length > 1 ? string.Join(" ", args.Skip(1).ToArray()) : "Hello World. (Default message)";
+
+				var body = Encoding.UTF8.GetBytes(message);
+				channel.BasicPublish(exchangeName, routingKey, default, body);
+
+				Console.WriteLine($"Sent message with log level {routingKey}: {message}");
+
+				Console.WriteLine("Press [enter] to exit");
+				Console.ReadLine();
+			}
+		}
+	}
+
+	private static string GetMessage(string[] args)
+	{
+		return args.Length > 0 ? string.Join(" ", args) : "Hello World. (Default message)";
+	}
+}
